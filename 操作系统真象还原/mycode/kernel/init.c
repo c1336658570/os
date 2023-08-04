6b2809ef5a34b80fd09dbae8526d503ee6480b02
c1336658570 <1336658570@qq.com>
2023-08-04 12:02:37 +0800

    add锁,有bug

--- 操作系统真象还原/mycode/kernel/init.c
+++ 操作系统真象还原/mycode/kernel/init.c
@@ -4,6 +4,7 @@
 #include "../device/timer.h"
 #include "memory.h"
 #include "thread.h"
+#include "console.h"
 
 //初始化所有模块
 void init_all() {
@@ -12,4 +13,5 @@ void init_all() {
   timer_init(); //初始化PIT
   mem_init();	  // 初始化内存管理系统
   thread_init();  //初始化线程相关结构
+  console_init(); //控制台初始化最好放在开中断之前
 }
