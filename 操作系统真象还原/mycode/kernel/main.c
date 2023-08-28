067e6cf485b4e298f85b6e927bda6b3c00bae725
c1336658570 <1336658570@qq.com>
2023-08-28 15:12:56 +0800

    实现sys_lseek

--- 操作系统真象还原/mycode/kernel/main.c
+++ 操作系统真象还原/mycode/kernel/main.c
@@ -31,30 +31,38 @@ int main(void) {
    printf("1_ read %d bytes:\n%s\n", read_bytes, buf);
 
    memset(buf, 0, 64);
-   read_bytes = sys_read(fd, buf, 6);
+   read_bytes = sys_read(fd, buf, 64);
    printf("2_ read %d bytes:\n%s", read_bytes, buf);
 
    memset(buf, 0, 64);
    read_bytes = sys_read(fd, buf, 6);
    printf("3_ read %d bytes:\n%s", read_bytes, buf);
 
-   printf("________  close file1 and reopen  ________\n");
-   sys_close(fd);
-   fd = sys_open("/file1", O_RDWR);
+   printf("________  SEEK_SET 0  ________\n");
+   sys_lseek(fd, 0, SEEK_SET);
    memset(buf, 0, 64);
    read_bytes = sys_read(fd, buf, 24);
    printf("4_ read %d bytes:\n%s", read_bytes, buf);
-   
    memset(buf, 0, 64);
    read_bytes = sys_read(fd, buf, 64);
    printf("5_ read %d bytes:\n%s", read_bytes, buf);
+  
+   sys_lseek(fd, -60, SEEK_END);
+   memset(buf, 0, 64);
+   read_bytes = sys_read(fd, buf, 64);
+   printf("6_ read %d bytes:\n%s", read_bytes, buf);
 
+   sys_lseek(fd, -60, SEEK_CUR);
+   memset(buf, 0, 64);
+   read_bytes = sys_read(fd, buf, 64);
+   printf("7_ read %d bytes:\n%s", read_bytes, buf);
+   
    sys_close(fd);
    while(1);
    return 0;
 }
 
-//在线程中运行的函数
+/* 在线程中运行的函数 */
 void k_thread_a(void* arg) {     
    void* addr1 = sys_malloc(256);
    void* addr2 = sys_malloc(255);
@@ -75,7 +83,7 @@ void k_thread_a(void* arg) {
    while(1);
 }
 
-//在线程中运行的函数
+/* 在线程中运行的函数 */
 void k_thread_b(void* arg) {     
    void* addr1 = sys_malloc(256);
    void* addr2 = sys_malloc(255);
@@ -96,7 +104,7 @@ void k_thread_b(void* arg) {
    while(1);
 }
 
-//测试用户进程
+/* 测试用户进程 */
 void u_prog_a(void) {
    void* addr1 = malloc(256);
    void* addr2 = malloc(255);
@@ -111,7 +119,7 @@ void u_prog_a(void) {
    while(1);
 }
 
-//测试用户进程
+/* 测试用户进程 */
 void u_prog_b(void) {
    void* addr1 = malloc(256);
    void* addr2 = malloc(255);
